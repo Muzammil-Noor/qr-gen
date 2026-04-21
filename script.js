b9b6f68ed493809f0ca6bc17531f1c17208b92ab
Muzammil <muzammilnoor897@gmail.com>
2026-04-21 21:15:16 +0500

    Update
    Better Popup
    Size Selection
    Live Update
    Offline

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
-const input = document.getElementById("inputText");
-const button = document.getElementById("generateBtn");
-const qrImage = document.getElementById("qrImage");
+let qr;
 
-button.addEventListener("click", () => {
-  const data = encodeURIComponent(input.value.trim());
+const input = document.getElementById("data");
+const sizeSelect = document.getElementById("size");
 
-  if (!data) {
-    alert("Please enter some text");
-    return;
-  }
+function generateQR() {
+    const value = input.value.trim();
+    const size = parseInt(sizeSelect.value);
 
-  const url = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${data}`;
-  qrImage.src = url;
+    document.getElementById("qr").innerHTML = "";
+
+    if (!value) return;
+
+    qr = new QRCode(document.getElementById("qr"), {
+        text: value,
+        width: size,
+        height: size
+    });
+}
+
+input.addEventListener("input", generateQR);
+sizeSelect.addEventListener("change", generateQR);
+
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs[0]?.url) {
+        input.value = tabs[0].url;
+        generateQR();
+    }
 });
+
+const params = new URLSearchParams(window.location.search);
+const dataFromMenu = params.get("data");
+
+document.getElementById("downloadBtn").addEventListener("click", () => {
+    const canvas = qrContainer.querySelector("canvas");
+
+    if (!canvas) return;
+
+    const link = document.createElement("a");
+    link.download = "qr-code.png";
+    link.href = canvas.toDataURL("image/png");
+    link.click();
+});
+console.log("script?")
+if (dataFromMenu) {
+    input.value = dataFromMenu;
+    generateQR();
+}
